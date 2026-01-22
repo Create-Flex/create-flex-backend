48d0bbffc51066d5cbe2fdc7d501ec2519f72753
ICEH0TCH0C0 <teg0806@naver.com>
2026-01-22 16:24:22 +0900

    feat: first setting #1

---
+++ in4/src/test/java/com/mcn/in4/In4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcn.in4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class In4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
