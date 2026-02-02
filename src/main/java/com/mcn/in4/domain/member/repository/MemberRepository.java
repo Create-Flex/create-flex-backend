121a4b5f039ee803125c371494f2b93e8e2da1a9
LordOfHun <38762989+LordOfHun@users.noreply.github.com>
2026-02-02 12:39:26 +0900

    memberRepository merge
    memberRepository merge

--- src/main/java/com/mcn/in4/domain/member/repository/MemberRepository.java
+++ src/main/java/com/mcn/in4/domain/member/repository/MemberRepository.java
@@ -1,6 +1,7 @@
 package com.mcn.in4.domain.member.repository;
 
 import com.mcn.in4.domain.member.entity.Member;
+import com.mcn.in4.domain.member.entity.memberEnum.MemberRole;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
