27fd3b0a92bfedda57600137a67d886240487430
LordOfHun <38762989+LordOfHun@users.noreply.github.com>
2026-02-24 16:38:19 +0900

    feat: 기타 수정 1 #275

--- src/main/java/com/mcn/in4/domain/QnA/service/QAServiceImpl.java
+++ src/main/java/com/mcn/in4/domain/QnA/service/QAServiceImpl.java
@@ -13,7 +13,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.mcn.in4.domain.QnA.dto.QAResponseDto.*;
